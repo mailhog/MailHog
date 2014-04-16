6b119ea573dee2a0d6847d5cdcdc8f3a0339f382
Ian Kent <email@iankent.co.uk>
2014-04-17 00:11:56 +0100

    Read from socket

--- mailhog/smtp/smtp.go
+++ mailhog/smtp/smtp.go
@@ -18,16 +18,37 @@ type Message struct {
 	Helo string
 }
 
-func StartSession(conn *net.TCPConn) (*Session) {
+func StartSession(conn *net.TCPConn) {
 	conv := &Session{conn}
 	conv.Begin()
-	return conv
+}
+
+func (c Session) Read() {
+	buf := make([]byte, 1024)	
+	n, err := c.conn.Read(buf)
+	if n == 0 {
+		log.Printf("Connection closed by remote host\n")
+		return
+	}
+	if err != nil {
+		log.Printf("Error reading from socket: %s\n", err)
+		return
+	}
+	text := string(buf)
+	log.Printf("Received %d bytes: %s\n", n, text)
+	c.Parse(text)
+}
+
+func (c Session) Parse(content string) {
+	log.Printf("Parsing string: %s", content)
+	c.Read()
 }
 
 func (c Session) Begin() {
 	_, err := c.conn.Write([]byte("220 Go-MailHog\n"))
 	if err != nil {
-		log.Printf("Failed writing to socket: %s", err)
+		log.Printf("Failed writing to socket: %s\n", err)
 		return
 	}
+	c.Read()
 }
