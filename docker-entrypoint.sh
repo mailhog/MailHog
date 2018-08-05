5e0a85fa499a40d8d8570cc561b3fd88cdf56045
damlys <damian.lysiak@gmail.com>
2018-08-05 22:39:04 +0200

    add docker entrypoint and http basic auth envs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ if [ -n "$HTTP_BASIC_AUTH_USER" ] && [ -n "$HTTP_BASIC_AUTH_PASSWORD" ]
 then
     echo "Generating password file: /home/mailhog/htpasswd"
     echo "$HTTP_BASIC_AUTH_USER:$(MailHog bcrypt $HTTP_BASIC_AUTH_PASSWORD)" > /home/mailhog/htpasswd
-    MailHog -auth-file=/home/mailhog/htpasswd
+    exec "MailHog -auth-file=/home/mailhog/htpasswd $@"
 else
-    MailHog
+    exec "MailHog $@"
 fi
 
 exit 0
